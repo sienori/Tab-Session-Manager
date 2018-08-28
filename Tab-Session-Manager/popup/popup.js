35082caca7d92773de890ae1fb0fc24b6d055f38
Kike Cabrera <kikecalpe@gmail.com>
2018-08-28 21:16:21 +0200

    Minor bug

--- Tab-Session-Manager/popup/popup.js
+++ Tab-Session-Manager/popup/popup.js
@@ -955,8 +955,9 @@ document.addEventListener("click", async function(e) {
   hideAllPopupMenu(e);
   switch (e.target.id) {
     case "panel":
-      var createData = { type: "detached_panel", url: "panel.html", width: 550, height: 500};
+      var createData = { type: "detached_panel", url: "../popup/panel.html", width: 650, height: 650};
       var creating = browser.windows.create(createData);
+      break;
     case "setting":
       browser.runtime.openOptionsPage();
       break;
