c5c2e5c97a08a90fa502b80c7b411a06bfe10ded
sienori <iilll7d6f@yahoo.co.jp>
2018-10-17 23:49:22 +0900

    Delete update notification

--- src/background/onInstalledListener.js
+++ src/background/onInstalledListener.js
@@ -1,19 +1,10 @@
 import browser from "webextension-polyfill";
-import { initSettings, getSettings } from "src/settings/settings";
+import { initSettings, getSettings, setSettings } from "src/settings/settings";
 
 const openOptionsPage = active => {
   browser.tabs.create({
     url: "options/index.html#information?action=updated",
     active: active
-  });
-};
-
-const openNotification = () => {
-  browser.notifications.create({
-    type: "basic",
-    iconUrl: "../icons/icon.png",
-    title: browser.i18n.getMessage("NotificationOnUpdateLabel"),
-    message: browser.i18n.getMessage("NotificationOnUpdateCaptionLabel")
   });
 };
 
@@ -25,11 +16,6 @@ export default async details => {
   if (isShowOptionsPage) {
     openOptionsPage(false);
   } else {
-    openNotification();
-    const handleNotificationClick = () => {
-      openOptionsPage(true);
-      browser.notifications.onClicked.removeListener(handleNotificationClick);
-    };
-    browser.notifications.onClicked.addListener(handleNotificationClick);
+    setSettings("isShowUpdated", true);
   }
 };
