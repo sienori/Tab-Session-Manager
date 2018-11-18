5fa7bfa3471768317fcce9e587be64f53ec6f93e
sienori <iilll7d6f@yahoo.co.jp>
2018-11-19 00:34:41 +0900

    Fix undefined variable

--- src/background/updateOldSessions.js
+++ src/background/updateOldSessions.js
@@ -44,7 +44,7 @@ const migrateSessionsFromStorage = async () => {
   };
   let sessions = await getSessionsByStorage();
   if (sessions.length == 0) return;
-  log.log(logDir, "=>migrateSessionsFromStorage() sessions", session);
+  log.log(logDir, "=>migrateSessionsFromStorage() sessions", sessions);
 
   //タグを配列に変更
   const updateTags = () => {
