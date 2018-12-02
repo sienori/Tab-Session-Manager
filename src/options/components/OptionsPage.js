0f0a0ec6dc8fd900996f57cd2dcdff16a9177c43
sienori <iilll7d6f@yahoo.co.jp>
2018-12-02 21:20:58 +0900

    Make the option page side bar position fixed

--- src/options/components/OptionsPage.js
+++ src/options/components/OptionsPage.js
@@ -2,15 +2,18 @@ import React from "react";
 import { HashRouter } from "react-router-dom";
 import SideBar from "./SideBar";
 import ContentsArea from "./ContentsArea";
+import ScrollToTop from "./ScrollToTop";
 import "../styles/OptionsPage.scss";
 
 export default () => {
   return (
     <HashRouter hashType="noslash">
-      <div className="optionsPage">
-        <SideBar />
-        <ContentsArea />
-      </div>
+      <ScrollToTop>
+        <div className="optionsPage">
+          <SideBar />
+          <ContentsArea />
+        </div>
+      </ScrollToTop>
     </HashRouter>
   );
 };
