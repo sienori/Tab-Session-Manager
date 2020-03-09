07bab5ebf1bef7d415c6f11a77d8095f1fca5851
sienori <iilll7d6f@yahoo.co.jp>
2020-03-10 00:10:28 +0900

    Update importSessions using lastEditedTime

--- src/background/import.js
+++ src/background/import.js
@@ -1,4 +1,3 @@
-import uuidv4 from "uuid/v4";
 import log from "loglevel";
 import Sessions from "./sessions.js";
 import { saveSession } from "./save.js";
@@ -7,29 +6,17 @@ const logDir = "background/import";
 
 export default async function importSessions(importedSessions) {
   log.log(logDir, "import()", importedSessions);
-  //idを無視して文字列に変換
-  const toString = session => {
-    let retSession = {};
-    Object.assign(retSession, session);
-    delete retSession.id;
-    return JSON.stringify(retSession);
-  };
-
-  //同一のセッションが存在するか判定
-  const isExistSameSession = (session, currentSessions) => {
-    for (let currentSession of currentSessions) {
-      if (toString(session) === toString(currentSession)) return true;
-    }
-    return false;
-  };
 
   //同一セッションが存在しなければインポートする
   for (let importedSession of importedSessions) {
     const currentSessions = await Sessions.search("date", importedSession.date);
 
-    if (isExistSameSession(importedSession, currentSessions)) continue;
+    const isSameSession = session =>
+      session.id == importedSession.id && session.lastEditedTime <= importedSession.lastEditedTime;
+    const existsSameSession = currentSessions.some(isSameSession);
+    if (existsSameSession) return;
 
-    importedSession.id = uuidv4();
+    importedSession.lastEditedTime = Date.now();
     saveSession(importedSession);
   }
 }
