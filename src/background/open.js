d2c9fdbf4674b45d374fb47683443f5a57fbd456
sienori <iilll7d6f@yahoo.co.jp>
2021-04-18 22:53:47 +0900

    Apply save tab gropus setting when open session

--- src/background/open.js
+++ src/background/open.js
@@ -126,7 +126,7 @@ const createTabGroups = async (windowId, tabs, tabGroupsInfo) => {
     }, groupId => {
       const groupInfo = tabGroupsInfo.find(info => info.id === group.originalGroupId);
       if (!groupInfo) return;
-      updateTabGroups(groupId, groupInfo);
+      if (getSettings("saveTabGroups")) updateTabGroups(groupId, groupInfo);
     });
   }
 };
